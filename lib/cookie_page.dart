c663ede0dc8610172900117294f6087f029a972f
sirdeelord <saddyodus@yahoo.com>
2020-01-24 01:45:17 -0500

    init commit

---
+++ lib/cookie_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CookiePage extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Color(0xFFFCFAF8),
+            body: ListView(
+                
+            ),
+        );
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/bottom_bar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+    // This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home: MyHomePage(),
+        );
+    }
+}
+
+class MyHomePage extends StatefulWidget {
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> 
+    with SingleTickerProviderStateMixin {
+    TabController _tabController;
+
+    @override
+    void initState() {
+        super.initState();
+        _tabController = TabController(length: 3, vsync: this);
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.white,
+                elevation: 0.0,
+                centerTitle: true,
+                leading: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+                    onPressed: () {},
+                ),
+            title: Text(
+                'Pickup',
+                style: TextStyle(
+                fontFamily: 'Varela', fontSize: 20.0, color: Color(0xFF545D68)),
+            ),
+            actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+                    onPressed: () {})
+            ],
+        ),
+        body: ListView(
+            padding: EdgeInsets.only(left: 20.0),
+            children: <Widget>[
+                SizedBox(height: 15.0),
+                Text(
+                    'Categories',
+                    style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 42.0,
+                        fontWeight: FontWeight.bold
+                    )
+                ),
+                SizedBox(height: 15.0),
+                TabBar(
+                    controller: _tabController,
+                    indicatorColor: Colors.transparent,
+                    labelColor: Color(0xFFC88D67),
+                    isScrollable: true,
+                    labelPadding: EdgeInsets.only(right: 45.0),
+                    unselectedLabelColor: Color(0xFFADACAC),
+                    tabs: [
+                        Tab(
+                            child: Text('Cookies',
+                                style: TextStyle(
+                                    fontFamily: 'Varela',
+                                    fontSize: 21.0
+                                )
+                            )
+                        ),
+                        Tab(
+                            child: Text('Cookie cake',
+                                style: TextStyle(
+                                    fontFamily: 'Varela',
+                                    fontSize: 21.0
+                                )
+                            )
+                        ),
+                        Tab(
+                            child: Text('Ice cream',
+                                style: TextStyle(
+                                    fontFamily: 'Varela',
+                                    fontSize: 21.0
+                                )  
+                            )
+                        ),
+                    ],
+                )
+            ],
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {},
+        backgroundColor: Color(0xFFF17532),
+        child: Icon(Icons.fastfood)
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomBar(),
+    );
+  }
+}
