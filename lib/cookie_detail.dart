36c81319d2986fadbe82b86248d5b3382ab373c1
sirdeelord <saddyodus@yahoo.com>
2020-01-27 23:30:43 -0500

    detail ui created

---
+++ lib/cookie_detail.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/bottom_bar.dart';
+
+class CookieDetail extends StatelessWidget {
+    final assetPath, cookieprice, cookiename;
+
+    CookieDetail({this.assetPath, this.cookiename, this.cookieprice});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.white,
+                elevation: 0.0,
+                centerTitle: true,
+                leading: IconButton(
+                    icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+                    onPressed: () {
+                        Navigator.of(context).pop();
+                    },
+                ),
+                title: Text(
+                    'Pickup',
+                    style: TextStyle(
+                    fontFamily: 'Varela', fontSize: 20.0, color: Color(0xFF545D68)),
+                ),
+                actions: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+                        onPressed: () {})
+                ],
+            ),
+
+            body: ListView(
+                children: [
+                    SizedBox(height: 15.0),
+                    Padding(
+                        padding: EdgeInsets.only(left: 20.0),
+                        child: Text('Cookie',
+                            style: TextStyle(
+                            fontFamily: 'Valera',
+                            fontSize: 42.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFFF17532)
+                        )),
+                    ),
+                    SizedBox(height: 15.0),
+                    Hero(
+                        tag: assetPath,
+                        child: Image.asset(
+                            assetPath, 
+                            height: 150.0,
+                            width: 100.0,
+                            fit: BoxFit.contain
+                        ),
+                    ),
+                    SizedBox(height: 20.0)
+                ]
+            ),
+
+            floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Color(0xFFF17532),
+                child: Icon(Icons.fastfood)
+            ),
+            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+            bottomNavigationBar: BottomBar(),
+        );
+    }
+}
